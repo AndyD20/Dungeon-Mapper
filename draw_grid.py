65e1ebce0d2949992622918f5ca59bee710b9430
Andy <adelaney2011@hotmail.co.uk>
2019-01-22 21:18:07 +0000

    Draw simple grid on canvas

--- draw_grid.py
+++ draw_grid.py
@@ -1,11 +1,27 @@
+from pygame import draw
 
-shape = __import__('program_variables').program_variables.shape
+shape = __import__('program_variables').ProgramVariables.shape
+tile_size = __import__('program_variables').ProgramVariables.tile_size
+grey = __import__('program_variables').ProgramVariables.Colours.grey
 
 
 def draw_grid(display_surface):
     display_surface.fill((255, 255, 255))
 
-    for x in range(0, shape[0]):
+    if shape[0] == shape[1]:
+        for x in range(0, shape[0]):
+            if x % tile_size == 0:
+                draw.line(display_surface, grey, (x, 0), (x, shape[0] - 1), 1)
+                draw.line(display_surface, grey, (0, x), (shape[0] - 1, x), 1)
+
+    else:
+        for x in range(0, shape[0]):
+            if x % tile_size == 0:
+                draw.line(display_surface, grey, (x, 0), (x, shape[1] - 1), 1)
+
         for y in range(0, shape[1]):
+            if y % tile_size == 0:
+                draw.line(display_surface, grey, (0, y), (shape[0] - 1, y), 1)
+
 
 
--- program_variables.py
+++ program_variables.py
@@ -3,3 +3,6 @@ class ProgramVariables:
     tile_size = 10
     window_title = 'Dungeon Mapper'
     game_state = None
+
+    class Colours:
+        grey = (211, 211, 211)
